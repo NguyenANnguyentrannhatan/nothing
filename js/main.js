5efdf026e96aa87d470580b6cca59aa64d14d3fa
NguyenANnguyentrannhatan <523H0115@student.tdtu.edu.vn>
2025-10-27 22:44:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girlfriends Day 💓').split('')
+    const titles = ('TUYN - 32300428').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
